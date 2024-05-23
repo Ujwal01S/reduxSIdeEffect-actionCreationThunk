8cf461b35e5592e0b92c79dd2bddf62e8bcf64f1
Ujwal Suwal <snow7neanderthal477@gmail.com>
2024-05-23 20:50:22 +0545

    added fetch and new cart-action for http post and fetch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector, useDispatch } from 'react-redux';
-import { sendCartData } from './store/cart-slice';
+import { sendCartData, fetchCartData } from './store/cart-actions';
 import Notification from './components/UI/Notification';
 
 let isInitial= true; // I am writting this initial here because it only initialize when
@@ -18,11 +18,17 @@ function App() {
   const notification= useSelector(state=> state.ui.notification);
 
   useEffect(()=>{
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(()=>{
     if (isInitial){
       isInitial= false;
       return;
     }
-    dispatch(sendCartData(cart));
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
   
   return (
